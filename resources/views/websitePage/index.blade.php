f18fa3028b662c3c8b8141323b61a6f8f1eac43c
FTS-Dev3 <131987160+FTS-Dev3@users.noreply.github.com>
2024-10-04 02:00:33 +0500

    add js and css

--- resources/views/websitePage/index.blade.php
+++ resources/views/websitePage/index.blade.php
@@ -18,14 +18,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="{{asset('assets/landingpage/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/landingpage/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/landingpage/aos/aos.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/landingpage/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/landingpage/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="{{asset('assets/landingpage/main.css')}}" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: Squadfree
@@ -554,7 +554,7 @@
       <div class="credits">
         Designed by <a href="https://firmtechservices.com/">FTS</a>
       </div>
-    </div>
+    </div>asset
 
   </footer>
 
@@ -565,17 +565,18 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="{{asset('assets/landingpage/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('assets/landingpage/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('assets/landingpage/aos/aos.js')}}"></script>
+  <script src="{{asset('assets/landingpage/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{asset('assets/landingpage/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('assets/landingpage/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+  <script src=" {{asset('assest/landingpage/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('assets/landingpage/swiper/swiper-bundle.min.css')}}"></script>
+
 
   <!-- Main JS File -->
-  <script src="assets/js/main.js"></script>
+  <script src="{{asset('assets/landingpage/js/main.js')}}"></script>
 
 </body>
 
